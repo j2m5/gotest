20498c9b98c795df24e95e97518250809c582e11
Дмитрий <45686133+j2m5@users.noreply.github.com>
2026-06-08 22:40:51 +0300

    refactor for spa

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,12 +7,6 @@ import (
 )
 
 func Register(h *handlers.Handler, m *middleware.Middleware) {
-	http.HandleFunc("/", m.Auth(h.Home))
-	http.HandleFunc("/register", m.Guest(h.Register))
-	http.HandleFunc("/login", m.Guest(h.Login))
-	http.HandleFunc("/logout", m.Auth(h.Logout))
-	http.HandleFunc("/verify", h.VerifyEmail)
-
 	http.HandleFunc("/api/auth/register", h.Register)
 	http.HandleFunc("/api/auth/login", h.Login)
 	http.HandleFunc("/api/auth/logout", m.Auth(h.Logout))
